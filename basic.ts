3470903909b7b3dd7d256d2168995d4b662f5029
jason taekyun Kim <saurus234@gmail.com>
2022-06-02 03:55:25 +0900

    readonly property

--- basic.ts
+++ basic.ts
@@ -1,9 +1,10 @@
+type Age = number;
 type Player = {
-  name: string;
-  age?: number; //옵셔널타입 ? 으로 선택적으로 값을 줄수 있음
+  readonly name: string; //readonly 속성을 지정할 수 있음
+  age?: Age; //옵셔널타입 ? 으로 선택적으로 값을 줄수 있음
 };
 
-//자주 사용하는 타입을 빼서 선언해줄수 있음
+//자주 사용하는 타입을 빼서 Alias로 선언해줄수 있음
 const jason: Player = {
   name: "jason",
 };
@@ -12,3 +13,16 @@ const serena: Player = {
   name: "serena",
   age: 20,
 };
+
+//argument 타입 설정
+//return 값의 타입 = Player
+function playerMaker(name: string): Player {
+  return {
+    name: name,
+  };
+}
+
+const playerSet = (name: string): Player => ({ name });
+
+const jake = playerMaker("jake");
+jake.age = 12;
